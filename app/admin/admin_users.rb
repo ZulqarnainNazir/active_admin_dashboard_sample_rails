5a27620690f59b59953192fadda20d043b5c4d77
Zulqarnain nazir <zulqarnainnazir1@gmail.com>
2018-02-15 17:38:54 +0500

    admin dashboar

---
+++ app/admin/admin_users.rb
@@ -0,0 +1,36 @@
+ActiveAdmin.register AdminUser do
+  permit_params :email, :password, :password_confirmation
+
+  index do
+    selectable_column
+    id_column
+    column :email
+    column :current_sign_in_at
+    column :sign_in_count
+    column :created_at
+    actions
+  end
+
+  filter :email
+  filter :current_sign_in_at
+  filter :sign_in_count
+  filter :created_at
+
+# add new admin without sending email
+  # form do |f|
+  #   f.inputs do
+  #     f.input :email
+  #     f.input :password
+  #     f.input :password_confirmation
+  #   end
+  #   f.actions
+  # end
+# add new admin with password then send email with invitation link and set password from there
+  form do |f|
+    f.inputs "Add new Admin Email"do
+      f.input :email
+    end
+    f.actions
+  end
+
+end
---
+++ app/admin/books.rb
@@ -0,0 +1,26 @@
+ActiveAdmin.register Book do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+
+#  to select specific coloumn to show use this
+  # index do
+  #   selectable_column
+  #   id_column
+  #   column :name
+  #   column :price
+  #   column :author
+  #   column :created_at
+  #   actions
+  # end
+end
